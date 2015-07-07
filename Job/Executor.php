dc846f4c28459f1f259564afd595d5588f3d7e39
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2015-07-07 01:22:53 +0000

    feat(executor): issue warning for nonexporting tables

--- Job/Executor.php
+++ Job/Executor.php
@@ -76,7 +76,7 @@ class Executor extends BaseExecutor
         foreach ($tables as $table) {
 
             if (!$writer->isTableValid($table)) {
-                //@todo: create warning event in SAPI
+                $this->logger->info("Table {$table["tableId"]} not exported.");
                 continue;
             }
 
